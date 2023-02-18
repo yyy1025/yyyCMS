b91cef1ed4aabc8e3e78f89ef122c2230248ea93
yyy <2484218565@qq.com>
2023-02-19 01:13:45 +0800

    feat(router): 未登录不能进主页

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import { localCache } from '@/utils/cache'
 //大家都喜欢使用函数,传入对象，返回router
 //使用hash模式看起来舒服
 //routes数组添加path和component之间的映射关系
@@ -24,5 +25,17 @@ const router = createRouter({
       component: () => import('../views/not-found/notFound.vue')
     }
   ]
+})
+//添加导航守卫
+router.beforeEach((to, from) => {
+  //return的是路由地址，但是我们可以判断to和from
+  //不返回或者返回undefined，进行默认跳转
+  if (to.path == '/main') {
+    const token = localCache.getCache('token')
+    if (token) {
+      return '/main'
+    }
+    return '/login'
+  }
 })
 export default router
